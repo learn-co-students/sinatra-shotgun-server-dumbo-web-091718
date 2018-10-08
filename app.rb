1e822f8a65428bc2b4071248258e306642fd7a5b
github username <github email address>
2018-10-08 00:25:55 -0400

    Shotgun

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
  #acts as controller
 class App < Sinatra::Base
 
-  get '/' do  # Get route
+  get '/' do  # Get route as root-path
     "Started my server using Shotgun!"
   end
 
